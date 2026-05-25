550c078c3f273df6142a94557e3bb3fbf559e47c
walkerulrich <167113306+walkerulrich@users.noreply.github.com>
2026-05-25 17:31:11 +0200

    feat: adding requirements file

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["3.144.212.231"]/' /home/ubuntu/project_devops/agency/settings.py
+sed -i 's/\[]/\["44.197.133.230"]/' /home/ubuntu/project_devops/agency/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
